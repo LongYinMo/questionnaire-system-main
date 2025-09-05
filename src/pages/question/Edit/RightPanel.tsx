5e1f0b5bea07ae463f47810b69ca2c67c740c5eb
龙吟墨 <1952711685@qq.com>
2025-09-06 01:38:13 +0800

    页面切换bug修复-3

--- src/pages/question/Edit/RightPanel.tsx
+++ src/pages/question/Edit/RightPanel.tsx
@@ -3,7 +3,7 @@
  * @Author      zono
  * @Description 右侧面板
  * */
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { Tabs } from 'antd'
 import { FileTextOutlined, SettingOutlined } from '@ant-design/icons'
 import ComponentProp from './ComponentProp'
@@ -17,10 +17,10 @@ enum TAB_KEYS {
 }
 
 const RightPanel: FC = () => {
-  
-  
-
-  
+  const { selectedId } = useGetComponentInfo()
+  const [activeKey, setActiveKey] = useState(
+    selectedId ? TAB_KEYS.PROP_KEY : TAB_KEYS.SETTING_KEY
+  )
 
   const tabsItems = [
     {
@@ -37,7 +37,13 @@ const RightPanel: FC = () => {
     },
   ]
 
-  return <Tabs items={tabsItems}></Tabs>
+  const handleTabChange = (key: string) => {
+    if (Object.values(TAB_KEYS).includes(key as TAB_KEYS)) {
+      setActiveKey(key as TAB_KEYS);
+    }
+  };
+
+  return <Tabs activeKey={activeKey} items={tabsItems} onChange={handleTabChange}></Tabs>
 }
 
 export default RightPanel
